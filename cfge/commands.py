bd4ccdd3255c30c8ee820526e07201a0a0f1cdb9
Mike Matera <matera@lifealgorithmic.com>
2015-11-05 15:50:38 -0800

    LVM commands kina suck

--- cfge/commands.py
+++ cfge/commands.py
@@ -1,5 +1,17 @@
 
 import cfge
+import re
+
+def mounts() : 
+    out = cfge.check_output(["mount"])
+    mnt = {}
+    mnt['by_device'] = {}
+    mnt['by_mount'] = {}
+    for line in out.splitlines(True) : 
+        s = line.split()
+        mnt['by_device'][s[0].decode()] = s[2].decode()
+        mnt['by_mount'][s[2].decode()] = s[0].decode()
+    return mnt;
 
 def readtab(cmd, idcol=0, headers=None, sep=None) :
     r = {}
@@ -22,7 +34,13 @@ def ps() :
 
 def lvmstat() :
     r = {}
-    r['lvs'] = readtab(cmd=['lvs', '-o', 'lv_all', '--separator', '!:!'], sep='!:!')
+    r['lvs'] = readtab(cmd=['lvs', '-o', 'lv_all,vg_all', '--separator', '!:!'], sep='!:!')
     r['vgs'] = readtab(cmd=['vgs', '-o', 'vg_all', '--separator', '!:!'], sep='!:!', idcol=2)
     r['pvs'] = readtab(cmd=['pvs', '-o', 'pv_all', '--separator', '!:!'], sep='!:!', idcol=3)
+    # add some helpful info to lvs output 
+    for lv in r['lvs'].values() : 
+        lv['mountpath'] = "/dev/mapper/" \
+                         + re.sub('-', '--', lv['vg']) \
+                         + "-" + re.sub('-', '--', lv['lv'])
+
     return r
