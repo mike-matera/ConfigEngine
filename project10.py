8dc959e0c7963b9bc7d4e776bd465283f331d2c5
Mike Matera <matera@lifealgorithmic.com>
2015-12-03 12:33:32 -0800

    No partial credit for just doing the lab

--- project10.py
+++ project10.py
@@ -6,41 +6,47 @@ import cfge
 import cfge.commands 
 
 engine = cfge.instance()
+score = 0; 
 
 print ("\n---- This is the project 10 grader ----\n")
 
-kernel = engine.check_output(["uname", "-r"]).decode().strip()
-print ('Your kernel version is:', kernel);
-parts = kernel.split('-')
-if len(parts) < 3 : 
-    print ("ERROR: Your kernel doesn't seem to have an extra version string")
+def do_kernel() : 
+    kernel = engine.check_output(["uname", "-r"]).decode().strip()
+    print ('Your kernel version is:', kernel);
+    parts = kernel.split('-')
+    if len(parts) < 3 : 
+        print ("ERROR: Your kernel doesn't seem to have an extra version string")
 
-print ("\tUpstream kernel version:", parts[0]);
-print ("\tBuild number:", parts[1]);
-print ("\tExtra version:", parts[2]);
-print ("");
+        print ("\tUpstream kernel version:", parts[0]);
+        print ("\tBuild number:", parts[1]);
+        print ("\tExtra version:", parts[2]);
+        print ("");
 
-score = 0; 
+    if parts[2] == 'generic': 
+        print ("ERROR: You do not seem to have customized this kernel.")
+        return False
+    else :
+        print ("SUCCESS: This kernel is customized (+10)")
+        return True;
 
-if parts[2] == 'generic': 
-    print ("ERROR: You do not seem to have customized this kernel.")
-else :
-    print ("SUCCESS: This kernel is customized (+10)")
+def do_module() :
+    print ("\nLooking for scull kernel module.");
+    modules = engine.check_output(["lsmod"]).decode()
+    found = False
+    for line in modules.splitlines(True) :
+        parts = line.split(' '); 
+        if parts[0] == 'scull' :
+            print ("FOUND:", line.strip())
+            return True
+
+        print ("ERROR: The scull driver is not loaded.")
+        return False
+
+score = 0 
+if do_kernel() : 
     score += 10
-
-print ("\nLooking for scull kernel module.");
-modules = engine.check_output(["lsmod"]).decode()
-found = False
-for line in modules.splitlines(True) :
-    parts = line.split(' '); 
-    if parts[0] == 'scull' :
-        print ("FOUND:", line.strip())
-        found = True
+    if do_module() :
         score += 10
-        break;
-
-if not found :
-    print ("ERROR: The scull driver is not loaded.")
 
 print ("\n\nYour score for is", score, "of 20\n");
 
